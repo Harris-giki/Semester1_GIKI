dda817e5e477519e3807f8467ab76df5fe755837
Afnan Bin Abbas <afnanbabbas@gmail.com>
2025-01-25 14:22:16 +0500

    CS101 updated

---
+++ CS101/Sample Codes/Patterns/floyds triangle.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	int counter=1;
+	
+	for(int i=1; i<=5; i++)
+	{
+		for(int j=1; j<=i; j++)
+		cout<<counter<<" ";
+		counter++;
+	    cout<<endl;
+	}
+		
+}
