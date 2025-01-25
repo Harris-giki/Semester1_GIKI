1e4eca1e79b4e36e7ab1e1e70c5c469ebc2e9f16
Afnan Bin Abbas <afnanbabbas@gmail.com>
2025-01-25 14:24:55 +0500

    CS101 updated

---
+++ CS101/Sample Codes/Patterns/rectangle.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	//RECTANGLE PATTERN
+	
+	int rows,columns;
+	
+	cout<<"Enter no: of rows:  ";
+	cin>>rows;
+	
+	cout<<endl<<"Enter no: of columns: ";
+	cin>>columns;
+	
+	for(int i=1; i<=rows; i++)
+	{
+		for(int j=1; j<=columns; j++)
+		{
+		
+		cout<<"*";
+	}
+	
+	cout<<endl;
+}
+           
+
+
+}
