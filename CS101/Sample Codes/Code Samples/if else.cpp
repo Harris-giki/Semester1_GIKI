8ebfdc14c6ea31103d550a41156151966911e783
Afnan Bin Abbas <afnanbabbas@gmail.com>
2025-01-28 11:39:10 +0500

    Commit for file: if else.cpp

---
+++ CS101/Sample Codes/Code Samples/if else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+using namespace std;
+
+int main(){
+	
+	int savings;
+	cin>>savings;
+	
+	if (savings>500){
+		cout<<"Neha/n"
+	}
+	else{cout<<"Reshma\n"
+	}
+	
+	return 0;
+	
+}
