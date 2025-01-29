384ede82a90396553879e7deff89a73c4dea3e09
Afnan Bin Abbas <afnanbabbas@gmail.com>
2025-01-29 10:57:55 +0500

    Commit for file: 1 odd even.cpp

---
+++ CS101/Sample Codes/if else/1 odd even.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	
+	cout<<"Enter a number: ";
+	cin>>num;
+	cout<<endl;
+	
+	if( num % 2 == 0)
+	{
+		cout<<"EVEN NUMBER";
+	}
+	
+	else 
+	{
+		cout<<"ODD NUMBER";
+	}
+	
+	return 0;
+}
