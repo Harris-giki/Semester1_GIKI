02fc0b0216a53759f87f11fb97ffaee115cda530
Afnan Bin Abbas <afnanbabbas@gmail.com>
2025-02-03 22:47:00 +0500

    Commit for file: HCF 2.cpp

---
+++ CS101/Sample Codes/Programs in class/HCF 2.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	int gcd;
+	
+	cin>>a,b;
+	
+	for(int i=1; i<=a && i<=b; i++)
+	{
+		if(a%i==0 && b%i==0)
+		{
+			gcd = i;
+		}
+	}
+	cout<<endl<<gcd;
+}
